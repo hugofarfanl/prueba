6fd4e71a41c283467f4003f2f68cdab2293d1f4e
Hugo <hugoe.farfanl@konradlorenz.edu.co>
2025-05-15 13:39:59 -0500

    configuración deploy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,6 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: '/inicio', pathMatch: 'full' },
     {path:'inicio',component:LoginComponent}
 ];
