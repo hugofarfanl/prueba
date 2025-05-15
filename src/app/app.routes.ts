2e62ddf6fef0d1aaf225f3c24e983e58f489b671
Hugo <hugoe.farfanl@konradlorenz.edu.co>
2025-05-15 13:16:01 -0500

    configuración firebase

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+
+export const routes: Routes = [
+    {path:'inicio',component:LoginComponent}
+];
